0f87ff97b25c9dfaa2a3b5f4c8beb998c2897043
Hammad Gul <iamhammad148@gmail.com>
2026-04-03 15:54:53 +0500

    fixing import grocery json directly

--- controllers/groceryController.js
+++ controllers/groceryController.js
@@ -1,18 +1,15 @@
-import path from "path"
-import fs from 'fs'
+import groceryData from "../data/grocery/grocery.json" assert { type: "json" }
 import groceryModel from "../models/groceryModel.js"
 
-async function groceryController(req , resp){
-    try{
-        let groceryJsonPath = path.join(process.cwd() , "data/grocery/grocery.json")
-        let readGroceryFile = JSON.parse(fs.readFileSync(groceryJsonPath , "utf-8"))
+async function groceryController(req, resp) {
+    try {
         await groceryModel.deleteMany({})
-        let insertedData = await groceryModel.insertMany(readGroceryFile)
+        let insertedData = await groceryModel.insertMany(groceryData)
         return resp.status(200).json(insertedData)
     }
-    catch(e){
-        return resp.status(400).json({message : "Failed To Fetch Grocery"})
+    catch (e) {
+        return resp.status(400).json({ message: "Failed To Fetch Grocery" })
     }
 }
 
-export {groceryController}
+export { groceryController }
